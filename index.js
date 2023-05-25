e45914c491aa73e39a3c6e21c21d77ce14a84219
PranavKengale <pranavkengale76@gmail.com>
2023-05-25 15:31:55 -0400

    second  commit

---
+++ index.js
@@ -0,0 +1,60 @@
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+
+
+
+// routes
+import AuthRoute from './Routes/AuthRoute.js'
+import UserRoute from './Routes/UserRoute.js'
+import PostRoute from './Routes/PostRoute.js'
+import UploadRoute from './Routes/UploadRoute.js'
+// import ChatRoute from './routes/ChatRoute.js'
+// import MessageRoute from './routes/MessageRoute.js'
+
+const app = express();
+
+
+// To serve images for public
+app.use(express.static('public'))
+app.use('/images',express.static("images"))
+
+// middleware
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+// to serve images inside public folder
+app.use(express.static('public')); 
+app.use('/images', express.static('images'));
+
+
+dotenv.config();
+const PORT = process.env.PORT;
+
+
+
+const CONNECTION =process.env.MONGO_DB;
+mongoose
+  .connect(CONNECTION, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT || 5000, () => console.log('Listening at Port')))
+  .catch((error) => console.log(`${error} did not connect`));
+
+
+app.use('/auth', AuthRoute);
+app.use('/user', UserRoute)
+app.use('/posts', PostRoute)
+app.use('/upload', UploadRoute)
+// app.use('/chat', ChatRoute)
+// app.use('/message', MessageRoute)
+
+
+
+
+
+
+
+
+
+
